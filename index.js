9a095b672ae9b976ab2e18a72be97a95c7dd50e7
jc <jc>
2017-11-07 14:06:55 -0600

    e

---
+++ index.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+
+console.log("Your full name is" firstName)
