1e13947858e13daf89fc6420951ffa3abdf92805
jc <jc>
2017-11-08 10:47:19 -0600

    updated

--- index.js
+++ index.js
@@ -1,6 +1,83 @@
 // JavaScript source code
-var firstName = prompt("What is your first name?");
-var lastName = prompt("What is your last name?");
-var age = prompt("How old are you?");
 
-console.log("Your full name is" firstName)
+//########################################
+//#    While Loops Problem Set           #
+//########################################
+
+//Print all the numbers between -10 and 19
+//var num = -10;
+//while (num <= 19) {
+//    console.log(num);
+//    num++;
+//}
+
+//Print all even numbers between 10 and 40
+//var num = 10;
+//while (num <= 40) {
+//    if (num % 2 === 0) {
+//        console.log(num);
+//    }
+//    num++;
+//}
+
+//Print all odd numbers between 300 and 333
+//var num = 300;
+//while (num <= 333) {
+//    if (num % 2 !== 0) {
+//        console.log(num);
+//    }
+//    num++;
+//}
+
+//Print all numbers divisble by 5 AND 3 between 5 and 50
+//var num = 5;
+//while (num >= 5 && num <= 50) {
+//    if (num % 5 === 0 && num % 3 === 0) {
+//        console.log(num);
+//    }
+//    num++;
+//}
+
+//########################################
+//#    Higher Level Functions            #
+//########################################
+
+//function sing() {
+//    console.log("twinkle twinkle....");
+//    console.log("how I wonder...");
+//}
+
+////setInterval(anotherFunction, Interval)
+////Interval is in milliseconds (ms)
+
+//setInterval(sing, 1000);
+////to clear setInterval, call: clearInterval(number), where number is the number that was returned when sing was called
+
+//setInterval(function() {
+//    console.log("I am an anonymous function!");
+//    console.log("This is awesome!")
+//},2000)
+
+//########################################
+//#               Arrays                 #
+//########################################
+
+//var colors = ["red", "orange", "yellow"];
+
+//########################################
+//#               Arrays Methods         #
+//########################################
+
+//Push and Pop
+//Push referrs to adding something to the end of an array
+
+//var colors = ["red", "orange", "yellow"];
+//colors.push("green");
+
+//Shift and Unshift are the opposit of Push and Pop
+//Use unshift to add to the front of an array.
+//Use shift to remove the first item in an array.
+
+//########################################
+//#               Arrays Exercises       #
+//########################################
